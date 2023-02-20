00970c12ed48ab5634fc00efe88d1076e84a6bdc
aramistamboulian961 <aramistamboulian961@gmail.com>
2023-02-20 18:08:43 +0200

    fixed image problem

--- src/sections/services/index.js
+++ src/sections/services/index.js
@@ -4,19 +4,19 @@ import aboutService from '../../static/Data/aboutData'
 const Services = () => {
   return (
     <div className='services'>
-      <div className='py-20'>
+      <div className='my-20'>
         <div className='text-center'>
           <h1 className='text-3xl'>Services we provide</h1>
           <h2 className='text-2xl stroke py-3 font-light'>We build design that build your business</h2>
-          <p className='stroke'>We're a full-service web agency specializing in custom website design, development, <br /> maintenance, optimization, and marketing. Our expert team creates beautiful, user-friendly websites <br /> that drive results.</p>
+          <p className='stroke text-'>We're a full-service web agency specializing in custom website design, development, <br /> maintenance, optimization, and marketing. Our expert team creates beautiful, user-friendly websites <br /> that drive results.</p>
         </div>
-        <div className='imgContainer'>
-          <div className='cards float-left py-10'>
+        <div className='imgContainer my-5'>
+          <div className='img grid grid-cols-4'>
             {
               aboutService.map((items) => {
                 return (
-                  <div className='imgHolder'>
-                    <img src={items.img} alt="" />
+                  <div>
+                    <img className='imgs' src={items.img} alt="" width={500} />
                   </div>
                 )
               })
--- src/static/Data/aboutData/index.js
+++ src/static/Data/aboutData/index.js
@@ -2,8 +2,9 @@ const aboutService = [
     {
         id: '1',
         numbers: '1',
-        img: '/media/images/div.png',
-        // title: 'Brand Identity',
+        img: '/media/images/div (3).png',
+        icon: '',
+        title: 'Brand Identity + Positioning',
         // description: 'we specialize in helping businesses establish a strong and consistent brand identity that resonates with their target audience.',
 
     },
@@ -11,23 +12,27 @@ const aboutService = [
     {
         id: '2',
         numbers: '1',
+        icon: '',
         img: '/media/images/div.png',
-        // title: 'Web Design',
+        title: 'Web Design + Development',
 
     },
 
     {
         id: '3',
         numbers: '1',
-        img: '/media/images/div.png',
-        // title: 'Mobile Design',
+        icon: '',
+        img: '/media/images/div (2).png',
+        title: 'Mobile Design',
 
     },
 
     {
         id: '4',
         numbers: '1',
-        img: '/media/images/div.png',
+
+        img: '/media/images/div (1).png',
+        title: 'Advertising + Branded Content'
 
     },
 
