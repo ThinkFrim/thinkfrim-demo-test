5596dc054ba50effc05ea745de564a7817606f78
aramistamboulian961 <aramistamboulian961@gmail.com>
2023-02-27 22:36:25 +0200

    removed import Nav & cards from home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,4 @@
 import './home.css';
-import Nav from "../../components/nav";
-// import Cards from '../../components/cards';
 import Services from '../../sections/services';
 import Banner from '../../sections/banner';
 import LatestProjects from '../../sections/ourwork';
