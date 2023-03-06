aa67aeaebfd866990d33532e7a98429ff05d3885
aramistamboulian961 <aramistamboulian961@gmail.com>
2023-03-06 11:00:39 +0200

    fixed banner button + text responsive

--- src/components/ContactCard/index.js
+++ src/components/ContactCard/index.js
@@ -4,10 +4,10 @@ const ContactCard = () => {
     return (
         <div className='chineseBlackBg h-[100vh]'>
             <div className='flex flex-col items-center pt-20'>
-                <div className='Box RajahBg w-1/2 flex items-center justify-between  px-14'>
-                    <h1 className='chineseBlack text-3xl'>Do you have Any Project?</h1>
-                    <img src="/media/illustrator/footerDesing.svg" alt="" />
-                    <button className='contactUsBtn darkGreenBg White text-2xl'>Contact Us</button>
+                <div className='Box RajahBg w-1/2 max-sm:w-[100vw] max-sm:p-[3rem] flex items-center justify-between max-sm:justify-center px-14'>
+                    <h1 className='chineseBlack text-3xl max-sm:text-xl'>Do you have Any Project?</h1>
+                    <img className='max-sm:hidden' src="/media/illustrator/footerDesing.svg" alt="" />
+                    <button className='contactUsBtn darkGreenBg White text-2xl max-sm:text-xl max-sm:w-[50vw] '>Contact Us</button>
                 </div>
             </div>
         </div>
--- src/sections/banner/index.js
+++ src/sections/banner/index.js
@@ -5,22 +5,22 @@ const Banner = () => {
         <section id='banner' className='root chineseBlackBg px-6'>
             <Nav />
             <div className='flex max-sm:flex-col max-md:flex-col max-md:justify-center items-center justify-between mx-20 py-24'>
-                <div className='content'>
-                    <h1 className='White text-5xl max-sm:text-[30px] max-sm:text-center py-4'>Transform your online image <br /> with a professional website.</h1>
-                    <h2 className='White text-3xl max-sm:text-[20px] max-sm:text-center py-6'>Get ahead of the competition with <br /> a custom-built website</h2>
-                    <div>
-                        <button className='RajahBg max-sm:text-lg chineseBlack text-2xl p-4 bannerButton'>Discover</button>
+                <div className="max-sm:flex max-sm:flex-col max-sm:justify-center">
+                    <div className="max-sm:w-[100vw]">
+                        <h1 className='White text-5xl max-sm:text-[25px] max-sm:text-center max-sm:leading-[2rem] py-5'>Transform your online image <br /> with a professional website.</h1>
+                        <h2 className='White text-3xl max-sm:text-[16px] max-sm:text-center py-5'>Get ahead of the competition with <br /> a custom-built website</h2>
                     </div>
+                    <button className='RajahBg max-sm:text-lg chineseBlack text-2xl p-4 bannerButton max-sm:item block max-sm:mx-auto max-sm:max-w-xs'>Discover</button>
                 </div>
                 <div>
-                    <img src="/media/illustrator/laptop and charts (2).svg" className="max-sm:w-40 max-sm:hidden" alt="" />
+                    <img  src="/media/illustrator/laptop and charts (2).svg" alt="" />
                 </div>
             </div>
         </section>
     )
 }
 
-export default Banner
+export default Banner;
 
 
 
