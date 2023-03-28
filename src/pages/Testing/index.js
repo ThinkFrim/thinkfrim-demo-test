41251dd2ad881dc38f471833d7118d083130e3ed
aramistamboulian961 <aramistamboulian961@gmail.com>
2023-03-28 16:48:02 +0300

    installed npm install netlify-cli -g

--- src/pages/Testing/index.js
+++ src/pages/Testing/index.js
@@ -9,12 +9,6 @@ const Testing = () => {
         setIsOpen(!isOpen);
     };
     return (
-        <div className='chineseBlackBg h-[100vh]'>
-            <div className="columns-2 py-5">
-                <div className="bg-red-600 p-5"></div>
-                <div className="bg-red-600 p-5 break"></div>
-            </div>
-        </div>
     )
 }
 
