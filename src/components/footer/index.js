9abcd7ff3dfcffcf9b09bb4f139e268e569b896b
aramistamboulian961 <aramistamboulian961@gmail.com>
2023-03-28 16:55:09 +0300

    fixed className problem

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -28,19 +28,3 @@ const Footer = () => {
 };
 
 export default Footer;
-
-{/* <div className="relative">
-  <img className="absolute bottom-0 left-0 w-full z-20 max-sm:hidden" src="/media/illustrator/LeftWave.svg" alt="LeftWave.svg" />
-  <div className="absolute bottom-[12rem] left-[10%] max-sm:hidden BlueGreen text-2xl leading-[5rem] z-30">
-    <a href="tel:+96171455206">+961 71455206</a>
-    <h1>Beirut, Lebanon</h1>
-    <a href="mailto:thinkfrim@gmail.com">thinkfrim@gmail.com</a>
-  </div>
-  <img className="absolute bottom-0 right-0 max-sm:hidden" src="/media/illustrator/right.svg" alt="RightWave.svg" />
-  <img className="absolute bottom-full right-[11%] z-0 max-sm:hidden" src="/media/illustrator/Iphone.svg" alt="" />
-  <div className="absolute bottom-0 z-40 border-t-[1px] w-full border-White flex px-[10%] gap-[3rem] font-semibold py-[10px] BlueGreen max-sm:text-[15px] max-sm:w-full">
-    <h1>&copy; 2023 ThinkFrim</h1>
-    <h1>Privacy Policy</h1>
-    <h1>Terms & Condition</h1>
-  </div>
-</div> */}
--- src/sections/banner/index.js
+++ src/sections/banner/index.js
@@ -4,13 +4,13 @@ const Banner = () => {
     return (
         <section id="banner" className="chineseBlackBg h-screen xl:h-screen">
             <Nav />
-            <div class="flex flex-col items-center lg:flex-row xl:flex-row xl:ml-[5%] py-10">
-                <div class="text-center md:text-center lg:text-left lg:w-1/2 xl:w-1/2 lg:pl-10 xl:pl-10">
-                    <h1 class="text-2xl White lg:text-3xl xl:text-5xl pb-4 font-semibold lg:leading-[3rem] xl:leading-[3.5rem]">Transform your online image<br class="hidden md:inline" /> with a professional website</h1>
-                    <h2 class="text-2xl White lg:text-2xl xl:text-3xl font-light">Get ahead of the competition <br /> with a custom-built website</h2>
-                    <button class="RajahBg w-[45%] 2xl:max-w-[30%] lg:max-w-[50%] xl:max-w-[40%] md:w-[30vw] font-medium text-chineseBlack text-xl p-4 rounded-[16px] mt-5">Discover</button>
+            <div className="flex flex-col items-center lg:flex-row xl:flex-row xl:ml-[5%] py-10">
+                <div className="text-center md:text-center lg:text-left lg:w-1/2 xl:w-1/2 lg:pl-10 xl:pl-10">
+                    <h1 className="text-2xl White lg:text-3xl xl:text-5xl pb-4 font-semibold lg:leading-[3rem] xl:leading-[3.5rem]">Transform your online image<br className="hidden md:inline" /> with a professional website</h1>
+                    <h2 className="text-2xl White lg:text-2xl xl:text-3xl font-light">Get ahead of the competition <br /> with a custom-built website</h2>
+                    <button className="RajahBg w-[45%] 2xl:max-w-[30%] lg:max-w-[50%] xl:max-w-[40%] md:w-[30vw] font-medium text-chineseBlack text-xl p-4 rounded-[16px] mt-5">Discover</button>
                 </div>
-                <img class="max-w-[60%] lg:w-[50%] xl:w-[35%] md:w-[50%] mt-5" src="/media/illustrator/laptop and charts (2).svg" alt="" />
+                <img className="max-w-[60%] lg:w-[50%] xl:w-[35%] md:w-[50%] mt-5" src="/media/illustrator/laptop and charts (2).svg" alt="" />
             </div>
         </section>
 
